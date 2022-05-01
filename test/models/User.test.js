bdab87948e910b38586a62c1011bc3038be98d4a
pe-ca <mr.gloser1@gmail.com>
2022-05-01 16:38:21 -0500

    Test Requerimiento 2: Las propiedades dateCreated y lastUpdated deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase User

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('./../../app/models/User')
 
 describe("Unit Tests for User class", () =>{
     test('Create an User Object', () =>{
-        const user = new User(1, "RicardoPerales", "Ricardo", "Bio", "datCreated", "lastUpdated")
+        const user = new User(1, "RicardoPerales", "Ricardo", "Bio")
 
         expect(user.id).toBe(1)
         expect(user.username).toBe("RicardoPerales")
         expect(user.name).toBe("Ricardo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("datCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
     })
 })
