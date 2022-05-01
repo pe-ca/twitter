4aea2cb1251fa5b05299ca4ce797b97adfff739f
pe-ca <mr.gloser1@gmail.com>
2022-05-01 16:26:33 -0500

    agregada prueba de creacion de objetos

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,5 +1,14 @@
 const User = require('./../../app/models/User')
 
 describe("Unit Tests for User class", () =>{
-    
+    test('Create an User Object', () =>{
+        const user = new User(1, "RicardoPerales", "Ricardo", "Bio", "datCreated", "lastUpdated")
+
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("RicardoPerales")
+        expect(user.name).toBe("Ricardo")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("datCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    })
 })
