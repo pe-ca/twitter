0756ba3dbf7bdbee96501484b65f190fb650205c
pe-ca <mr.gloser1@gmail.com>
2022-05-01 16:57:25 -0500

    Requerimiento 4: Cada objeto necesita los siguientes setters: setUsername y setBio, para actualizar dichas propiedades

--- app/models/User.js
+++ app/models/User.js
@@ -23,6 +23,14 @@ class User {
     get getLastUpdated(){
         return this.lastUpdated
     }
+
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+
+    set setBio(newBio){
+        this.bio = newBio
+    }
 }
 
 module.exports = User
