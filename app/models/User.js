cc6376c53779beefd3d5a7315ad50f5afa49c150
pe-ca <mr.gloser1@gmail.com>
2022-05-01 16:40:23 -0500

    Requerimiento 2: Las propiedades dateCreated y lastUpdated deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase User

--- app/models/User.js
+++ app/models/User.js
@@ -1,11 +1,11 @@
 class User {
-    constructor(id, username, name, bio, dateCreated, lastUpdated){
+    constructor(id, username, name, bio){
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdated = lastUpdated
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
     }
 }
 
