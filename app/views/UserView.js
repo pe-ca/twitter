f789d2043d0372879a10ea870c16d6c944273cc1
pe-ca <mr.gloser1@gmail.com>
2022-05-01 20:07:27 -0500

    requerimiento 4. Verifica que se pueda crear un objeto User, al enviar un payload con las siguientes propiedades: username, id y name

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -5,12 +5,13 @@ class UserView {
         if(payload === null){
             return { error: "El payload no existe" }
         }
-        if(payload.username != 'string' && payload.name != 'string' && payload.id != 'int'){
+        if(typeof payload.username !== 'string' && typeof payload.name !== 'string' && typeof payload.id !== 'int'){
             return { error: "Los valores de las propiedades necesitan tener un valor valido"}
         }
         if(payload.username === undefined || payload.name === undefined || payload.id === undefined){
             return { error: "Los valores de las propiedades necesitan tener un valor valido"}
         }
+        return UserService.create(payload.id, payload.username, payload.name)
     }
 }
 
