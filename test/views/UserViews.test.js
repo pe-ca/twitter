644f6a678fb3295c84da7bf9854400ce1aac0f8f
pe-ca <mr.gloser1@gmail.com>
2022-05-01 19:21:55 -0500

    Test 2. Valida que un payload contenga en alguna de las llaves username, name, o id un valor en null. Si hay un valor null regresa un objeto con la llave error y que indique el texto: necesitan tener un valor válido

--- test/views/UserViews.test.js
+++ test/views/UserViews.test.js
@@ -7,4 +7,11 @@ describe("tests for UserViews", () =>{
 
         expect(result.error).toMatch(/payload no existe/)
     })
+
+    test("Return an error object when try to create a new user with a payload with invalid properties", () =>{
+        const payload = { username: null, name:12, id: "id"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitan tener un valor valido/)
+    })
 })
